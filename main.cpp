7cc17ba4125e7702621f2edfe05e7e148ef12430
Diksha <diksha@macbookpro>
2025-10-26 21:01:47 -0700

    reading into vector and list

--- main.cpp
+++ main.cpp
@@ -2,11 +2,33 @@
 // IDE used: Vim/Terminal
 
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <iomanip>
 #include <chrono>
 using namespace std;
 
 int main() {
-    
+    auto start = high_resolution_clock::now();
+    ifstream fin("codes.txt");
+    string line;
+    vector<string> vect;
+    while(getline(fin, line))
+	vect.push_back(line);
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end - start);
+    cout << "Time taken: " << duration.count() << " milliseconds\n";
+
+    auto start1 = high_resolution_clock::now();
+    ifstream fin1("codes.txt");
+    list<string> list;
+    while(getline(fin1, line))
+        list.push_back(line);
+    auto end1 = high_resolution_clock::now();
+    auto duration1 = duration_cast<milliseconds>(end - start);
+    cout << "Time taken: " << duration1.count() << " milliseconds\n";
+
 
     return 0;
 }
