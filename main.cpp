692490c53e08a2d6c886a2423a2a3181ba398f5b
Diksha <diksha@macbookpro>
2025-10-26 20:51:19 -0700

    starter code copied

---
+++ main.cpp
@@ -0,0 +1,19 @@
+// COMSC-210 | Lab 25 | Diksha Tara Natesan
+// IDE used: Vim/Terminal
+
+#include <iostream>
+#include <chrono>
+using namespace std;
+
+int main() {
+    
+
+    return 0;
+}
+
+/* syntax examples:
+auto start = high_resolution_clock::now()
+auto end = high_resolution_clock::now()
+auto duration = duration_cast<milliseconds>(end - start)
+duration.count() references elapsed milliseconds
+*/
